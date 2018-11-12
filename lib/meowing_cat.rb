9edc5ff6b103bf1fdd4bb5c23c024e61c2e029da
a lan <bluenowte97@gmail.com>
2018-11-12 02:37:21 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,7 @@
-## code your solution here. 
+## code your solution here.
+class Cat
+  attr_accessor :name
+  def meow
+    puts "meow!" # Instance Method, a particular baby is able to cry
+    end
+end
